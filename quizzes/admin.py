dbef53c7a2373ecdf85d3a21885dffb918322d76
amirhoseinbidar <amirhoseinbk00@gmail.com>
2018-06-05 03:11:47 -0400

    i make database and make a admin manager for manage quizzes

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from quizzes import models
 # Register your models here.
+admin.site.register(models.grade)
+admin.site.register(models.lesson)
+admin.site.register(models.level)
+admin.site.register(models.quizze)
+admin.site.register(models.source)
