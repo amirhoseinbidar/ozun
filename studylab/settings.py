92ccd168a96d9d4137cdc1f384c936a36cdd729b
amirhoseinbidar <amirhoseinbk00@gmail.com>
2018-06-22 01:46:31 -0400

    update

--- studylab/settings.py
+++ studylab/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'amirhoseinbk00@gmail.com'
+EMAIL_HOST_PASSWORD = 'amir1380' #TODO: this is not secure should encypte
+EMAIL_PORT = 587
