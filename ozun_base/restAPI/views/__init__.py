b3225f334a4e3251b13d0db4b05262ecb80663c1
amirhoseinbidar <amirhoseinbk00@gmail.com>
2019-07-02 09:45:42 -0400

    update

--- ozun_base/restAPI/views/__init__.py
+++ ozun_base/restAPI/views/__init__.py
@@ -8,10 +8,10 @@ from .quiz_views import (
 from .exam import StartExam , FinishExam ,ExamInfo ,UpdateExam 
 
 from .studyPost import (
-    MagazineViewSet ,CourseViewSet ,
+    MagazineViewSet , CourseViewSet ,
     userProfileList , CourseSearch  ,
-    MagazineSearch , CourseFeedback , 
-    MagzineFeedback,
+    MagazineSearch  , CourseFeedback , 
+    MagzineFeedback ,
 )
 
 from restAPI.views.qa import (
